c00f3a7b31a1fef7209ae0a73f0bec30c7859585
Gustavo Trigolo <gustavotrigolo8@gmail.com>
2024-03-26 20:34:42 -0300

    quinto commit do App

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import iconsluxury from "./assets/icon-luxury.svg"
 
 export default function App() {
   return(
-  <main>
+  <main className="min-h-svh bg-offwhitebg py-20 px-6">
     <section>
 
       <div className="bg-orange">
