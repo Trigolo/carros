ca8510333c09ee91c0bf3128d94b3db5ce500974
Gustavo Trigolo <gustavotrigolo8@gmail.com>
2024-03-26 21:34:17 -0300

    projeto finalizado

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,11 @@ import Suvs from "./components/Suvs"
 export default function App() {
   return(
   <main className="min-h-svh bg-offwhitebg py-20 px-6 antialiased lg:flex justify-center items-center">
-    <section className="lg:flex max-w-[920px]">
+    <div className="lg:flex max-w-[920px]">
       <Sedans/>
       <Suvs/>
       <Luxuosos/>
-    </section>
+    </div>
   </main>
   )
 }
